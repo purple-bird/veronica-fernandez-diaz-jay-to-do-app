67e256c8ece1fb328bac3e324c159536638beb12
Veronica <179986051+purple-bird@users.noreply.github.com>
2025-03-31 22:57:08 -0600

    added todo list item component and a paragraph containing a value controlled by state

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css';
 import TodoList from './TodoList.jsx';
 import TodoForm from './TodoForm.jsx';
+import { useState } from 'react';
 
 function App() {
+  const [newTodo, setNewTodo] = useState(14);
   return (
     <div>
       <h1>Todo List</h1>
       <TodoForm />
+      <p>{newTodo}</p>
       <TodoList />
     </div>
   );
