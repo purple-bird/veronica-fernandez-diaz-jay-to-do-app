154464a00e4f2c3301fc0b85993bc7c6df431fb9
Veronica <179986051+purple-bird@users.noreply.github.com>
2025-04-16 00:42:16 -0600

    Week 05 Controlled Components and Forms

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,14 +1,26 @@
 import TodoListItem from './TodoListItem.jsx';
 
-function TodoList({ todoList }) {
+function TodoList({ todoList, onCompleteTodo }) {
+  const filteredTodoList = todoList.filter((todo) => !todo.isCompleted);
+
   return (
-    <div>
-      <ul>
-        {todoList.map((todo) => (
-          <TodoListItem key={todo.id} todo={todo} />
-        ))}
-      </ul>
-    </div>
+    <>
+      {filteredTodoList.length === 0 ? (
+        <p>Add todo above to get started</p>
+      ) : (
+        <>
+          <ul>
+            {filteredTodoList.map((todo) => (
+              <TodoListItem
+                key={todo.id}
+                todo={todo}
+                onCompleteTodo={onCompleteTodo}
+              />
+            ))}
+          </ul>
+        </>
+      )}
+    </>
   );
 }
 
