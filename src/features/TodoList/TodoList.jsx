f65a7ec9e41c63c9088dbc3875fa8be2c2fc6457
Veronica <179986051+purple-bird@users.noreply.github.com>
2025-04-30 02:07:00 -0600

    Week 07

--- src/features/TodoList/TodoList.jsx
+++ src/features/TodoList/TodoList.jsx
@@ -1,11 +1,13 @@
 import TodoListItem from './TodoListItem.jsx';
 
-function TodoList({ todoList, onCompleteTodo, onUpdateTodo }) {
+function TodoList({ todoList, onCompleteTodo, onUpdateTodo, isLoading }) {
   const filteredTodoList = todoList.filter((todo) => !todo.isCompleted);
 
   return (
     <>
-      {filteredTodoList.length === 0 ? (
+      {isLoading ? (
+        <p>Todo list loading...</p>
+      ) : filteredTodoList.length === 0 ? (
         <p>Add todo above to get started</p>
       ) : (
         <>
